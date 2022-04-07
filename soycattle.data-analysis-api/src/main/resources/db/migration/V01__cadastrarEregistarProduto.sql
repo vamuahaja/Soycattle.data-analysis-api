a7d33b5a46ce2ffa3b944af0e68a9fc0400f94eb
Nival <Nival@DESKTOP-HHUAO9U>
2022-04-07 15:59:36 +0200

    First data migration with flyway.

---
+++ soycattle.data-analysis-api/src/main/resources/db/migration/V01__cadastrarEregistarProduto.sql
@@ -0,0 +1,9 @@
+create table produto(
+    codigo bigint primary key,
+    nome varchar(20) not null
+);
+
+insert into produto values (40102,'Arroz');
+insert into produto values (40113,'Fumo');
+insert into produto values (40122,'Milho');
+insert into produto values (40124,'Soja');
